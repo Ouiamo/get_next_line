720bfde027fa5fc30369e1d9bf56a765642a73b4
Ouiamo <125459040+Ouiamo@users.noreply.github.com>
2024-01-01 00:04:26 +0100

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oaoulad- <oaoulad-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 09:37:39 by oaoulad-          #+#    #+#             */
+/*   Updated: 2023/12/31 21:37:19 by oaoulad-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE 
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+char	*got_noxt_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif

cf42c66ad30b2deb0cdf638138ef9185d3399473
Ouiamo <125459040+Ouiamo@users.noreply.github.com>
2024-01-01 00:02:25 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,20 +1,33 @@
-#include "get_next_line"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oaoulad- <oaoulad-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/31 09:50:16 by oaoulad-          #+#    #+#             */
+/*   Updated: 2023/12/31 23:54:40 by oaoulad-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char	*ft_read_file(int fd, char str_big)
+#include "get_next_line.h"
+
+char	*ft_read_file(int fd, char *str_big)
 {
 	char	*buff;
 	ssize_t	n;
 
 	buff = malloc(BUFFER_SIZE + 1);
-	if(!buff)
+	if (!buff)
 		return (NULL);
 	n = 1;
 	while (!ft_strchr(str_big, '\n') && (n != 0))
 	{
-		n = read(fd, buff, BUFFER_size);
+		n = read(fd, buff, BUFFER_SIZE);
 		if (n == -1)
 		{
 			free(buff);
+			free(str_big);
 			return (NULL);
 		}
 		buff[n] = '\0';
@@ -24,12 +37,14 @@ char	*ft_read_file(int fd, char str_big)
 	return (str_big);
 }
 
-char *my_line(char *str_big)
+char	*my_line(char *str_big)
 {
 	char	*line;
 	size_t	i;
 
 	i = 0;
+	if (!str_big[i])
+		return (NULL);
 	while (str_big[i] && str_big[i] != '\n')
 		i++;
 	line = malloc(i + 2);
@@ -67,14 +82,14 @@ char	*my_rest(char *str_big)
 	rest = malloc(ft_strlen(str_big) - i + 1);
 	if (!rest)
 		return (NULL);
+	i++;
 	j = 0;
 	while (str_big[i])
-		rest[j++] = str_big[i];
+		rest[j++] = str_big[i++];
 	rest[j] = '\0';
-	return (rest)
-
+	free(str_big);
+	return (rest);
 }
-
 
 char	*get_next_line(int fd)
 {
@@ -84,7 +99,7 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	str_big = ft_read_file(fd, str_big);
-	if (!str_big || str_big[0] = '\0')
+	if (!str_big)
 		return (NULL);
 	line = my_line(str_big);
 	str_big = my_rest(str_big);
